f7fa6b890aaf9b95810fb928591299ce66e1d96a
LEYujin <86396071+LEYujin@users.noreply.github.com>
2021-08-18 17:15:22 +0900

    Update heading_North30_.ino

--- heading_North30_/heading_North30_.ino
+++ heading_North30_/heading_North30_.ino
@@ -11,6 +11,18 @@ MLX90393 mlx;
 MLX90393::txyz data; //Create a structure, called data, of four floats (t, x, y, and z)
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("MLX90393 Read Example");
+  Wire.begin();
+  //Connect to sensor with I2C address jumpers set: A1 = 1, A0 = 0
+  //Use DRDY pin connected to A3
+  //Returns byte containing status bytes
+  byte status = mlx.begin();
+
+  //Report status from configuration
+  Serial.print("Start status: 0x");
+  if(status < 0x10) Serial.print("0"); //Pretty output
+  Serial.println(status, BIN);
   mlx.setGainSel(1);
   mlx.setResolution(0, 0, 0); //x, y, z
   mlx.setOverSampling(0);
@@ -30,19 +42,19 @@ void setup() {
 
 void loop() {
   mlx.readData(data); //Read the values from the sensor
-  float x = float(data.x) + 30 ;
-  float y = float(data.y) + 35 ;
-  Serial.print("mag : \t ");
+  float x = float(data.x) - 200;
+  float y = float(data.y) - 270;
   Serial.print(x); Serial.print("\t");
   Serial.print(y);  Serial.print("\t");
-  float heading = atan2(y, x);
+  float heading = atan2(y, x); 
   
   if (heading < 0)
     heading += 2 * M_PI ;
     
     int signal = heading ;
-    heading = heading * 180/M_PI ; 
-  
+    heading = heading * 180/M_PI ;
+    Serial.print(heading); Serial.print("\n");
+    
   if (20 <= heading && heading <= 40) //Go straight
   { 
     left.writeMicroseconds(1550);
@@ -91,6 +103,6 @@ void loop() {
     right.writeMicroseconds(1510); 
   }
     
-  delay(5000);
+  delay(500);
   
 }
